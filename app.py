be8f2511d06a6111ba821b78aa05cfa6fddc67d2
soumoks <soumoks@gmail.com>
2020-02-16 03:40:12 -0700

    Added validation for sendemail api

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bson.objectid import ObjectId
 from pprint import pprint
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+import os
 import json
 
 #Extending JSONencoder to support Object_ID returned by MongoDB
@@ -59,7 +60,7 @@ Get citizens that have more than 50% match in interests
 @cross_origin()
 def get_matching_citizens():
     volunteer = request.headers.get('X-volunteer')
-    vaibhav_interests = ['chess','sleeping','home building','garden walks']
+    vaibhav_interests = ['sleeping','home building','garden walks']
     arsalan_interests = ['music','politics','science','reading']
     senior_list = [post for post in posts.find()]
     if request.headers['X-volunteer'] == "Vaibhav":
@@ -88,7 +89,7 @@ def get_matching_citizens():
 @app.route("/sendemail", methods = ["POST"])
 @cross_origin()
 def send_email():
-    sendgrid_api_key = ''
+    sendgrid_api_key = os.environ.get('SENDGRID_API_KEY')
     if request.method == 'POST':
         """The returned data will be in the following format:
         {
@@ -100,37 +101,38 @@ def send_email():
         """
         data = request.get_json(force=True,cache=False)
         #data = data.__dict__
-        vol_email = str(data.get("volunteer_email"))
-        vol_name = str(data.get("volunteer_name"))
-        senior_name = str(data.get('senior_citizen').get('name'))
-        time_slot = str(data.get('time_slot'))
-        vol_content = "You have requested to visit "+ senior_name+" at a timeslot of "+time_slot+". \n Thank you for your interest and we will get back to you."
-        home_content = vol_name+' whose email is: '+vol_email+' has requested to be a Caring Companion for '+ senior_name+'.\n The time slot requested is: '+ time_slot+'.\n '+ vol_name+' is awaiting your response!'
-        messag_to_vol = Mail(
-            from_email='email@em4292.sourabh.org',
-            to_emails=vol_email,
-            subject='Thank you for requesting to be a Caring Companion!',
-            html_content=vol_content)
-        messag_to_home = Mail(
-            from_email='email@em4292.sourabh.org',
-            to_emails='thanmayee.mudigonda@gmail.com',
-            subject=vol_name+' has requested to be a Caring Companion!',
-            html_content=home_content)
-        try:
-            sg = SendGridAPIClient(sendgrid_api_key)
-            response_to_vol = sg.send(messag_to_vol)
-            response_to_home = sg.send(messag_to_home)
-            print(response_to_vol.status_code)
-            print(response_to_vol.body)
-            print(response_to_vol.headers)
-            print(response_to_home.status_code)
-            print(response_to_home.body)
-            print(response_to_home.headers)
-        except Exception as e:
-            print(e.message)
-        return jsonify({'ok': True, 'message': 'Email sent successfully!'}), 200
-    else:
-        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+        if data is not None:    
+            vol_email = str(data.get("volunteer_email"))
+            vol_name = str(data.get("volunteer_name"))
+            senior_name = str(data.get('senior_citizen').get('name'))
+            time_slot = str(data.get('time_slot'))
+            vol_content = "You have requested to visit "+ senior_name+" at a timeslot of "+time_slot+". \n Thank you for your interest and we will get back to you."
+            home_content = vol_name+' whose email is: '+vol_email+' has requested to be a Caring Companion for '+ senior_name+'.\n The time slot requested is: '+ time_slot+'.\n '+ vol_name+' is awaiting your response!'
+            messag_to_vol = Mail(
+                from_email='email@em4748.sourabh.org',
+                to_emails=vol_email,
+                subject='Thank you for requesting to be a Caring Companion!',
+                html_content=vol_content)
+            messag_to_home = Mail(
+                from_email='email@em4748.sourabh.org',
+                to_emails='thanmayee.mudigonda@gmail.com',
+                subject=vol_name+' has requested to be a Caring Companion!',
+                html_content=home_content)
+            try:
+                sg = SendGridAPIClient(sendgrid_api_key)
+                response_to_vol = sg.send(messag_to_vol)
+                response_to_home = sg.send(messag_to_home)
+                print(response_to_vol.status_code)
+                print(response_to_vol.body)
+                print(response_to_vol.headers)
+                print(response_to_home.status_code)
+                print(response_to_home.body)
+                print(response_to_home.headers)
+            except Exception as e:
+                print(e.message)
+            return jsonify({'ok': True, 'message': 'Email sent successfully!'}), 200
+        else:
+            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
 
 if __name__ == "__main__":
